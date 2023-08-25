cd308d4f89a4a6fe0c99747c28b020c15ce6c1af
Samuel Eastlund <eastlund.s@gmail.com>
2023-08-25 13:41:23 +0100

    Updating work on problem 18

--- Problem018.py
+++ Problem018.py
@@ -37,21 +37,10 @@ answer = 1313 Not correct! I can't just rearrange the triangle how I want to!
 
 import numpy as np
 
-TRIANGLE = """75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+TRIANGLE = """3
+7 4
+2 4 6
+8 5 9 3"""
 
 
 def split_on_lines(string):
@@ -79,10 +68,37 @@ def bubble_sort(array):
 
 def main():
     triangle = split_on_lines(TRIANGLE)
-    for x in triangle:
-        print(x)
-
-
+    for x in range(len(triangle)):
+        for y in range(len(triangle[x])):
+            print("Row", x, "column", y, "holds", triangle[x][y])
+            triangle[x][y] = int(triangle[x][y])
+    print(triangle)
+    largest_route_val = 0
+    left_val = 0
+    right_val = 0
+    for x in reversed(range(len(triangle) - 1)):
+        largest_step_val = 0
+        step_values = []
+        for n in range(len(triangle) - 1):
+            # if len(step_values) == x:
+            #     continue
+            try:
+                left_val = triangle[x][n] + triangle[x+1][n]
+                print("adding", triangle[x][n], "to", triangle[x+1][n], "for left val", left_val)
+                right_val = triangle[x][n] + triangle[x+1][n+1]
+                print("adding", triangle[x][n], "to", triangle[x+1][n+1], "for right val", right_val)
+            except IndexError:
+                pass
+            if left_val > right_val:
+                step_values.append(left_val)
+            else:
+                step_values.append(left_val)
+            print("ending with largest step val of", largest_step_val)
+        print("replacing row with", step_values)
+        triangle[x] = step_values
+    print(triangle[0])
+    print(triangle[1][0])
+    print(largest_route_val)
 
 
 if __name__ == '__main__':
